419751ae9f210d2069d52fb1b6a9e0e4d4e9b75b
orgar <orgarfunkel@gmail.com>
2018-10-07 22:09:10 +0300

    working version

--- app/controllers/thumbnail_controller.rb
+++ app/controllers/thumbnail_controller.rb
@@ -1,9 +1,11 @@
 require "open-uri"
 require 'mini_magick'
-
+require 'fileutils'
 
 class ThumbnailController < ApplicationController
   BACKGROUND_COLOR = 'black'
+
+# TODO to add comments
 
   # GET /thumbnail
   def ipad_thumbnail
@@ -11,59 +13,52 @@ class ThumbnailController < ApplicationController
     validation_ans = validate_params
     return render json: {message: validation_ans}, status: 400 if validation_ans
 
-    # TODO delete images
-    # TODO retries
-
-# TODO its double do I want this for the returned code ? probebly not
+    # TODO its double do I want this for the returned code ? probebly not
     download = download_file params[:url]
     if download[:exception]
       render json: { message: "Fail to download file from url. #{download[:exception].message}", backtrace: download[:exception].backtrace}, status: 400
     end
 
-# TODO last todo should be here with begin rescue
-    image =  MiniMagick::Image.open(download[:filename])
-
-    ratio = [
-      get_ipad_ratio(image[:width], params[:width]),
-      get_ipad_ratio(image[:height], params[:height])
-    ].min
-
-    new_image_name = "#{get_random_string}-#{params[:width]}-#{params[:height]}.jpeg"
-    # TODO add exception handeling
-    MiniMagick::Tool::Convert.new do |c|
-      c << image.path
-      c.gravity 'center'
-      c.background BACKGROUND_COLOR
-      c.resize "#{(image[:width]*ratio).to_i}x#{(image[:height]*ratio).to_i}"
-      c.extent "#{params[:width]}x#{params[:height]}"
-      c.format "jpeg"
-      c << "storage/#{new_image_name}"
+    begin
+      image =  MiniMagick::Image.open(download[:filename])
+      FileUtils.rm(download[:filename])
+    rescue Exception => e
+      render json: { message: "Fail to open image", backtrace: e.backtrace}, status: 500
+      return
     end
 
-    image.write "storage/#{new_image_name}"
+    begin
+      ratio = [
+        get_ipad_ratio(image[:width], params[:width]),
+        get_ipad_ratio(image[:height], params[:height])
+      ].min
 
+      new_image_name = "#{get_random_string}-#{params[:width]}-#{params[:height]}.jpeg"
 
-    send_file("storage/#{new_image_name}",
-      :filename => "thumbnail-#{params[:width]}-#{params[:height]}.jpeg",
-      :type => "mime/type")
+      image.combine_options do |c|
+        c.resize("#{(image[:width]*ratio).to_i}x#{(image[:height]*ratio).to_i}")
+        c.background(BACKGROUND_COLOR)
+        c.gravity('center')
+        c.extent "#{params[:width]}x#{params[:height]}"
+      end
+      image.format "jpeg"
+      image.write "storage/#{new_image_name}"
 
-
-    # render json: {a: 1}, status: :ok
-
+      send_file("storage/#{new_image_name}",
+        :filename => "thumbnail-#{params[:width]}-#{params[:height]}.jpeg",
+        :type => "mime/type")
+    rescue Exception => e
+      render json: { message: "Fail to manipulate the image", backtrace: e.backtrace}, status: 500
+      return
+    end
   end
 
 
-
-
   private
-
-
   def get_ipad_ratio original, wanted
     return 1 if wanted.to_i > original
     return wanted.to_f / original.to_f
   end
-
-
 
   def validate_params
     return "Missing one of the following parameters: url, width, height" unless params[:url] && params[:width] && params[:height]
@@ -85,6 +80,5 @@ class ThumbnailController < ApplicationController
   def get_random_string
     "#{[*('a'..'z')].sample(12).join}#{Time.new.to_i.to_s}"
   end
-
 
 end
